a9211b14c5cf070928e98cf26f6918581fd26ddc
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-01-15 22:38:43 +0700

    1
    1

--- Assets/LB3D/CrabMonster/Scripts/Rikayon.cs
+++ Assets/LB3D/CrabMonster/Scripts/Rikayon.cs
@@ -1,23 +1,133 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAnimationController : MonoBehaviour
+{
+
+    private bool isAttack;
+
+    // Tính khoản cách dí theo
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] Transform target;
+    [SerializeField] float radius = 20f;  // Điều chỉnh radius để kẻ thù dí theo người chơi ở khoảng cách xa hơn
+    [SerializeField] private Animator animator;
+
+    // Vị trí ban đầu của enemy
+    [SerializeField] Vector3 viTriBanDau;
 
 
-public class Rikayon : MonoBehaviour {
+    private bool isDead = false; // Trạng thái Enemy
 
-    public Animator animator;
+    // Tấn công
+    [SerializeField] private float attackDamage = 10f;
+    [SerializeField] private float attackCooldown = 2f;
+    [SerializeField] private float distanceAttack = 20f; // Điều chỉnh khoảng cách tấn công
+    private bool canAttack = true;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+  
+    public enum CharacterState
+    {
+        Normal, Attack, Die
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            animator.SetTrigger("Attack_1");
+    // Trạng thái hiện tại của kẻ thù
+    public CharacterState currentState;
+
+    private void Start()
+    {
+      
+        viTriBanDau = transform.position;
+
+    }
+
+    void Update()
+    {
+        if (currentState == CharacterState.Die || navMeshAgent == null || !navMeshAgent.isOnNavMesh)
+        {
+            return;
         }
 
-	}
+        // Khoảng cách đến player
+        var distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= radius)
+        {
+            if (navMeshAgent.isOnNavMesh)
+            {
+                navMeshAgent.SetDestination(target.position);
+            }
+
+            animator.SetFloat("isRun", navMeshAgent.velocity.magnitude);
+
+            if (distance <= distanceAttack && canAttack)
+            {
+                ChangeState(CharacterState.Attack);
+            }
+        }
+        else if (distance > radius)
+        {
+            if (navMeshAgent.isOnNavMesh)
+            {
+                navMeshAgent.SetDestination(viTriBanDau);
+            }
+
+            animator.SetFloat("isRun", navMeshAgent.velocity.magnitude);
+
+            if (distance < 1f)
+            {
+                animator.SetFloat("isRun", 0);
+            }
+            ChangeState(CharacterState.Normal);
+        }
+    }
+
+    private void ChangeState(CharacterState newState)
+    {
+        isAttack = false;
+
+        switch (currentState)
+        {
+            case CharacterState.Normal:
+                break;
+            case CharacterState.Attack:
+                break;
+            case CharacterState.Die:
+                break;
+        }
+
+        switch (newState)
+        {
+            case CharacterState.Normal:
+                break;
+
+            case CharacterState.Attack:
+                if (canAttack)
+                {
+                    int randomAttack = Random.Range(0, 2); 
+                    if (randomAttack == 0)
+                    {
+                        animator.SetTrigger("Attack"); 
+
+                       
+                    }
+                    if (randomAttack == 1)
+                    {
+                        animator.SetTrigger("Attack"); 
+                       
+
+                    }
+                }
+                break;
+
+            case CharacterState.Die:
+                animator.SetBool("isDie", true);
+                Destroy(gameObject, 5f);
+                StopAllCoroutines();
+                break;
+        }
+        currentState = newState;
+    }
+
+   
 }
