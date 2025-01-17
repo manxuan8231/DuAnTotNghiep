7cb7bd9ca66e672b804b258b15326649ca258eb6
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-01-17 21:05:49 +0700

    1
    1

--- Assets/Resoucers/Enemy/Craft/CrabMonster/Scripts/Rikayon.cs
+++ Assets/Resoucers/Enemy/Craft/CrabMonster/Scripts/Rikayon.cs
@@ -48,13 +48,105 @@ public class EnemyAnimationController : MonoBehaviour
         if (navMeshAgent == null || !navMeshAgent.isOnNavMesh)
             return;
 
+        HandleStateTransition();
+        //if (navMeshAgent == null || !navMeshAgent.isOnNavMesh)
+        //    return;
+
+        //var distanceToTarget = Vector3.Distance(target.position, transform.position);
+        //var distanceToOrigin = Vector3.Distance(transform.position, viTriBanDau);
+
+        //switch (currentState)
+        //{
+        //    case CharacterState.Sleep:
+        //        hasWokenUp = false;
+        //        if (distanceToTarget <= wakeUpRadius && !hasWokenUp)
+        //        {
+        //            hasWokenUp = true;
+        //            ChangeState(CharacterState.WakeUp);
+
+        //        }
+        //        break;
+
+        //    case CharacterState.WakeUp:
+        //        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("WakeUp"))
+        //        {
+        //            navMeshAgent.isStopped = true;
+        //            animator.SetTrigger("isWakeUp");
+        //            ChangeState(CharacterState.Idle);
+        //        }
+        //        break;
+
+        //    case CharacterState.Idle:
+        //        if (distanceToTarget <= radius)
+        //        {
+        //            ChangeState(CharacterState.Run);
+        //        }
+        //        break;
+
+        //    case CharacterState.Run:
+        //        if (distanceToTarget > radius)
+        //        {
+        //            ChangeState(CharacterState.Return);
+        //        }
+        //        else if (distanceToTarget <= distanceAttack)
+        //        {
+        //            animator.SetBool("isSleep", false);
+        //            animator.SetBool("Idle", false);
+
+        //            animator.ResetTrigger("isWakeUp");
+        //            ChangeState(CharacterState.Attack);
+        //        }
+        //        else
+        //        {
+        //            navMeshAgent.SetDestination(target.position);
+        //            animator.SetBool("isRun", true);
+        //        }
+        //        break;
+
+        //    case CharacterState.Attack:
+        //        if (distanceToTarget > distanceAttack)
+        //        {
+        //            ChangeState(CharacterState.Run);
+
+
+
+        //        }
+        //        else if (canAttack)
+        //        {
+        //            StartCoroutine(PerformAttack());
+        //        }
+        //        break;
+
+        //    case CharacterState.Return:
+        //        if (distanceToOrigin <= 1f)
+        //        {
+        //            animator.SetBool("isRun", false);
+        //            hasWokenUp = false;
+        //            ChangeState(CharacterState.Sleep);
+        //        }
+        //        else
+        //        {
+        //            navMeshAgent.SetDestination(viTriBanDau);
+        //            animator.SetBool("isRun", true);
+        //        }
+        //        break;
+        //    case CharacterState.Die:
+        //          if (currentHealth <= 0 && currentState != CharacterState.Die)
+        //        {
+        //            ChangeState(CharacterState.Die);
+        //        }
+        //        break;
+
+        //}
+    }
+    private void HandleStateTransition()
+    {
         var distanceToTarget = Vector3.Distance(target.position, transform.position);
         var distanceToOrigin = Vector3.Distance(transform.position, viTriBanDau);
 
         switch (currentState)
         {
             case CharacterState.Sleep:
-                hasWokenUp = false;
                 if (distanceToTarget <= wakeUpRadius && !hasWokenUp)
                 {
                     hasWokenUp = true;
@@ -65,7 +157,6 @@ public class EnemyAnimationController : MonoBehaviour
             case CharacterState.WakeUp:
                 if (!animator.GetCurrentAnimatorStateInfo(0).IsName("WakeUp"))
                 {
-                    navMeshAgent.isStopped = true;
                     animator.SetTrigger("isWakeUp");
                     ChangeState(CharacterState.Idle);
                 }
@@ -90,14 +181,17 @@ public class EnemyAnimationController : MonoBehaviour
                 else
                 {
                     navMeshAgent.SetDestination(target.position);
-                    animator.SetBool("isRun", true);
                 }
                 break;
 
             case CharacterState.Attack:
-                if (distanceToTarget > distanceAttack)
+                if (distanceToTarget > distanceAttack && distanceToTarget <= radius)
                 {
                     ChangeState(CharacterState.Run);
+                }
+                else if (distanceToTarget > radius)
+                {
+                    ChangeState(CharacterState.Return);
                 }
                 else if (canAttack)
                 {
@@ -108,38 +202,89 @@ public class EnemyAnimationController : MonoBehaviour
             case CharacterState.Return:
                 if (distanceToOrigin <= 1f)
                 {
-                    animator.SetBool("isRun", false);
-                    hasWokenUp = false;
                     ChangeState(CharacterState.Sleep);
                 }
                 else
                 {
                     navMeshAgent.SetDestination(viTriBanDau);
-                    animator.SetBool("isRun", true);
-                }
-                break;
-            case CharacterState.Die:
-                  if (currentHealth <= 0 && currentState != CharacterState.Die)
-                {
-                    ChangeState(CharacterState.Die);
                 }
                 break;
 
+            case CharacterState.Die:
+                // Không xử lý gì trong trạng thái Die
+                break;
         }
     }
-
     private IEnumerator PerformAttack()
     {
+        
         canAttack = false;
         animator.SetTrigger("Attack");
         yield return new WaitForSeconds(attackCooldown);
         canAttack = true;
     }
 
+    //private void ChangeState(CharacterState newState)
+    //{
+    //    if (currentState == newState) return;
+
+    //    animator.SetBool("isRun", false);
+    //    animator.SetBool("isSleep", false);
+
+    //    switch (newState)
+    //    {
+    //        case CharacterState.Sleep:
+    //            navMeshAgent.isStopped = true;
+    //            animator.SetBool("isSleep", true);
+    //            break;
+
+    //        case CharacterState.WakeUp:
+    //            navMeshAgent.isStopped = true;
+    //            animator.SetTrigger("isWakeUp");
+    //            break;
+
+    //        case CharacterState.Idle:
+    //            navMeshAgent.isStopped = true;
+    //            animator.SetBool("Idle", true);
+    //            break;
+
+    //        case CharacterState.Run:
+    //            navMeshAgent.isStopped = false;
+    //            animator.SetBool("isRun", true);
+    //            break;
+
+    //        case CharacterState.Attack:
+    //            navMeshAgent.isStopped = true;
+    //            break;
+
+    //        case CharacterState.Return:
+    //            navMeshAgent.isStopped = false;
+    //            animator.SetBool("isRun", true);
+    //            break;
+    //        case CharacterState.Die:  
+    //            animator.SetBool("isRun", false);
+    //            animator.SetBool("isSleep", false);
+    //            animator.SetBool("Idle", false);
+    //            animator.ResetTrigger("isWakeUp");
+    //            animator.ResetTrigger("Attack");
+
+    //            // Kích hoạt animation Die
+    //            animator.SetTrigger("Die");
+    //          navMeshAgent.isStopped = true;
+    //            Destroy(gameObject, 3f);
+    //            break;
+    //    }
+
+    //    currentState = newState;
+    //}
     private void ChangeState(CharacterState newState)
     {
-        if (currentState == newState) return;
+        if (currentState == newState)
+            return;
 
+        // Reset tất cả các trigger và trạng thái animation
+        animator.ResetTrigger("isWakeUp");
+        animator.ResetTrigger("Attack");
         animator.SetBool("isRun", false);
         animator.SetBool("isSleep", false);
 
@@ -148,6 +293,7 @@ public class EnemyAnimationController : MonoBehaviour
             case CharacterState.Sleep:
                 navMeshAgent.isStopped = true;
                 animator.SetBool("isSleep", true);
+                hasWokenUp = false;
                 break;
 
             case CharacterState.WakeUp:
@@ -173,23 +319,16 @@ public class EnemyAnimationController : MonoBehaviour
                 navMeshAgent.isStopped = false;
                 animator.SetBool("isRun", true);
                 break;
-            case CharacterState.Die:  
-                animator.SetBool("isRun", false);
-                animator.SetBool("isSleep", false);
-                animator.SetBool("Idle", false);
-                animator.ResetTrigger("isWakeUp");
-                animator.ResetTrigger("Attack");
 
-                // Kích hoạt animation Die
+            case CharacterState.Die:
+                navMeshAgent.isStopped = true;
                 animator.SetTrigger("Die");
-              navMeshAgent.isStopped = true;
                 Destroy(gameObject, 3f);
                 break;
         }
 
         currentState = newState;
     }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
