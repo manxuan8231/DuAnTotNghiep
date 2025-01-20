f438ba63d310912dcf9fda1fc36e02393b93157b
manxuan8231 <lydangxuanman05@gmail.com>
2025-01-20 18:45:03 +0700

    wd

---
+++ Assets/Resoucers/Tuong(statue)/prefab/SpamEnemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpamEnemy : MonoBehaviour
+{
+    public GameObject enemy1;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public GameObject enemy4;
+    public GameObject enemy5;
+    public GameObject enemy6;
+    public GameObject enemy7;
+
+    void Start()
+    {
+        enemy1.SetActive(false);
+        enemy2.SetActive(false);
+        enemy3.SetActive(false);
+        enemy4.SetActive(false);
+        enemy5.SetActive(false);
+        enemy6.SetActive(false);
+        enemy7.SetActive(false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            enemy1.SetActive(true);
+            enemy2.SetActive(true);
+            enemy3.SetActive(true);               
+            enemy4.SetActive(true);
+            enemy5.SetActive(true);
+            enemy6.SetActive(true);
+            enemy7.SetActive(true);
+            Debug.Log("đã spame");
+        }
+    }
+}
