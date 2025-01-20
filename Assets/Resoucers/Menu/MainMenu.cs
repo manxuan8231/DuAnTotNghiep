86c4ea649fa6df76a462ff26f0f9a8004e5c6adf
manxuan8231 <lydangxuanman05@gmail.com>
2025-01-20 15:36:24 +0700

    wd

---
+++ Assets/Resoucers/Menu/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject helpPanel;
+    public Slider volumeSlider;
+
+
+    void Start()
+    {
+        settingsPanel.SetActive(false);
+        helpPanel.SetActive(false);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Setting()
+    {
+        settingsPanel.SetActive(true);
+    }
+    public void Help()
+    {
+        helpPanel.SetActive(true);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+    public void HidePanel()
+    {
+        settingsPanel?.SetActive(false);
+        helpPanel?.SetActive(false);
+    }
+
+}
