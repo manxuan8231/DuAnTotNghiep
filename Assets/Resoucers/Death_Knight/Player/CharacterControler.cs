cf3a3eae8baf4df7d9ab3c7646009304b30c48ec
manxuan8231 <lydangxuanman05@gmail.com>
2025-01-22 18:48:21 +0700

    c

--- Assets/Resoucers/Death_Knight/Player/CharacterControler.cs
+++ Assets/Resoucers/Death_Knight/Player/CharacterControler.cs
@@ -35,7 +35,7 @@ public class CharacterController : MonoBehaviour
 
     private bool isMovementLocked = false; // Kiểm soát trạng thái "không di chuyển"
     private bool isECooldown = false; // Kiểm tra trạng thái hồi chiêu của phím E
-    private float turnSmoothVelocity;
+   
 
     public SliderHp sliderHp;
 
@@ -112,10 +112,8 @@ public class CharacterController : MonoBehaviour
         if (movement.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(movement.x, movement.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
-
-            // Làm mượt góc xoay nhân vật
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
+            Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
 
             Vector3 moveDirection = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
 
