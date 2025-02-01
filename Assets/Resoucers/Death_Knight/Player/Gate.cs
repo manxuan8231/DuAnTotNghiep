3378c57d888b86186b10d7f3fdd5c2fc423e3b74
manxuan8231 <lydangxuanman05@gmail.com>
2025-02-01 18:49:03 +0700

    wd

---
+++ Assets/Resoucers/Death_Knight/Player/Gate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gate : MonoBehaviour
+{
+    public int sceneOnLoad;
+    public Vector3 vector3;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Kiểm tra nếu Player chạm vào cổng
+        {
+            DontDestroyOnLoad(other.gameObject); // Giữ Player khi load Scene mới
+            SceneManager.LoadScene(sceneOnLoad); // Chuyển sang Scene2
+            other.transform.position = vector3;
+        }
+    }
+}
