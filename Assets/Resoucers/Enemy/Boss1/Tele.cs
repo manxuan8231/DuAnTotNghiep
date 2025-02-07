81b0886a3e39ddca15738e448ce285c8a67e747c
manxuan8231 <lydangxuanman05@gmail.com>
2025-02-07 13:47:44 +0700

    dw

---
+++ Assets/Resoucers/Enemy/Boss1/Tele.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tele : MonoBehaviour
+{
+    public GameObject buttonF;
+    public GameObject tele;
+    public GameObject player;
+    void Start()
+    {
+        buttonF.SetActive(false);
+    }
+
+   
+    void Update()
+    {
+        if(buttonF.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+              player.transform.position = tele.transform.position;
+              buttonF.SetActive(false);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            buttonF.SetActive(true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            buttonF.SetActive(false);
+        }
+    }
+}
