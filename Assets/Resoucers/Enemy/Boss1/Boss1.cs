363a5aeac72294977cfaaeccd6c2ca0c9ea08db6
manxuan8231 <lydangxuanman05@gmail.com>
2025-02-11 16:21:42 +0700

    s

--- Assets/Resoucers/Enemy/Boss1/Boss1.cs
+++ Assets/Resoucers/Enemy/Boss1/Boss1.cs
@@ -1,38 +1,45 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
 using Cinemachine;
-using System.Collections;
 
 public class Boss1 : MonoBehaviour
 {
     public Transform player;
+
+    // Xử lý chức năng launching
     public CinemachineVirtualCamera bossCam;
     public CinemachineVirtualCamera playerCam;
     public float detectRange = 10f;
     public float focusDuration = 3f;
     private bool isFocusing = false;
 
-
+    // Audio sounds
     public AudioSource audioSource;
     public AudioClip audioClipLaughVFX;
-    public NavMeshAgent navMeshAgent;
-    public Animator animator;
 
-   
+    // AI
+    public NavMeshAgent navMeshAgent;
+
+    // Xử lý tấn công
+    public float distancePlayer = 10f; // Khoảng cách nhìn thấy player để tấn công
+    public float attackCooldown = 10f; // Thời gian hồi chiêu
+    private float lastAttackTime = 0f;
+    public GameObject Weappon;
+    public Animator animator;
 
     private void Start()
     {
         playerCam.Priority = 20;
         bossCam.Priority = 0;
-       
+        Weappon.SetActive(false);
     }
 
     private void Update()
     {
-        CameraFocus();    
+        CameraFocus(); // Xử lý camera
+        Attack(); // Xử lý tấn công
     }
 
-  
     // Xử lý camera focus
     private void CameraFocus()
     {
@@ -41,12 +48,12 @@ public class Boss1 : MonoBehaviour
         if (distance <= detectRange && !isFocusing)
         {
             isFocusing = true;
-            animator.SetBool("isLaughing",true);
+            animator.SetBool("isLaughing", true);
             StartCoroutine(FocusOnBoss());
         }
     }
 
-    private IEnumerator FocusOnBoss()
+    private System.Collections.IEnumerator FocusOnBoss()
     {
         bossCam.Priority = 20;
         playerCam.Priority = 10;
@@ -75,6 +82,34 @@ public class Boss1 : MonoBehaviour
         if (audioSource != null && audioSource.isPlaying)
         {
             audioSource.Stop();
+        }
+    }
+
+    // Xử lý tấn công
+    private void Attack()
+    {
+        float distance = Vector3.Distance(transform.position, player.position);
+        if (distance <= distancePlayer && Time.time >= lastAttackTime + attackCooldown)
+        {
+            int random = Random.Range(0, 3);
+            if(random == 0)
+            {
+                Debug.Log("Thực hiện attack 1");
+                animator.SetTrigger("Attack");
+                Weappon.SetActive(true);
+            }
+                if (random == 1)
+            {
+                Debug.Log("Thực hiện attack 2");
+                animator.SetTrigger("Attack2");
+                Weappon.SetActive(true);
+            }
+                if(random == 2)
+            {
+                Debug.Log("Thực hiện dịch chuyển ra");
+
+            }
+            lastAttackTime = Time.time; // Cập nhật thời gian tấn công cuối cùng
         }
     }
 }
---
+++ Assets/Resoucers/Enemy/Boss1/Explosion1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(TakeHealth(other)); // Truyền other vào
+        }
+    }
+
+    public IEnumerator TakeHealth(Collider other)
+    {
+        SliderHp sliderHp = other.gameObject.GetComponent<SliderHp>(); // Lấy SliderHp từ Player
+
+        if (sliderHp == null)
+        {
+            Debug.LogWarning("Không tìm thấy SliderHp trên " + other.gameObject.name);
+            yield break;
+        }
+
+        yield return new WaitForSeconds(0.9f);
+        sliderHp.TakeDame(150);
+    }
+}
