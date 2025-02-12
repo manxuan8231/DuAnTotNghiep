8274aad1bdb279a76caa74cad511f63676559d9d
manxuan8231 <lydangxuanman05@gmail.com>
2025-02-12 15:11:56 +0700

    boss

--- Assets/Resoucers/Enemy/Boss1/Explosion1.cs
+++ Assets/Resoucers/Enemy/Boss1/Explosion1.cs
@@ -4,25 +4,39 @@ using UnityEngine;
 
 public class Explosion1 : MonoBehaviour
 {
+    public SliderHp sliderHp; // Tham chiếu đến SliderHp
+    private Coroutine takeHealthCoroutine; // Tham chiếu đến coroutine TakeHealth
+
+    private void Start()
+    {
+       
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            StartCoroutine(TakeHealth(other)); // Truyền other vào
+            takeHealthCoroutine = StartCoroutine(TakeHealth(other)); // Lưu tham chiếu đến coroutine
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (takeHealthCoroutine != null)
+            {
+                StopCoroutine(takeHealthCoroutine); // Dừng coroutine nếu người chơi thoát khỏi box
+            }
         }
     }
 
     public IEnumerator TakeHealth(Collider other)
     {
-        SliderHp sliderHp = other.gameObject.GetComponent<SliderHp>(); // Lấy SliderHp từ Player
-
-        if (sliderHp == null)
-        {
-            Debug.LogWarning("Không tìm thấy SliderHp trên " + other.gameObject.name);
-            yield break;
-        }
+        sliderHp = other.gameObject.GetComponent<SliderHp>(); // Lấy SliderHp từ Player
 
         yield return new WaitForSeconds(0.9f);
+
         sliderHp.TakeDame(150);
     }
 }
