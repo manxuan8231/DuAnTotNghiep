f5959f773353da90445f08068576f6e80469b38b
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-02-17 02:18:56 +0700

    1
    1

---
+++ Assets/Resoucers/InventoryAndItem/ItemPickUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public bool onRage;
+    void Start()
+    {
+        onRage = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && onRage == true)
+        {
+            Destroy(gameObject);
+            Debug.Log("đã nhặt");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onRage = true;
+            Debug.Log("Đã trong vùng");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            onRage = false;
+            Debug.Log("đã rời khỏi vùng");
+        }
+    }
+}
