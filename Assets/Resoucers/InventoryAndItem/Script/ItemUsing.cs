2d79f6c1da5bd661be942adead77fc66a699d75e
Minhtung0510 <169137996+Minhtung0510@users.noreply.github.com>
2025-03-04 00:56:23 +0700

    1
    1

--- Assets/Resoucers/InventoryAndItem/Script/ItemUsing.cs
+++ Assets/Resoucers/InventoryAndItem/Script/ItemUsing.cs
@@ -2,20 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class ItemUsing : MonoBehaviour
 {
     public SliderHp sliderHP;
-    public Item item;
-    public void UsingItemHeal()
+    //public Item item;
+    public void UsingItemHeal(Item item)
     {
-        Debug.Log("1");
-        sliderHP.GetHp(item.itemValue);
-
-        
-        
-
+        Debug.Log("using hp");
+        sliderHP.GetHp( item.itemValue);
     }
-    public void UsingItemMana()
+    public void UsingItemMana(Item item)
     {
         Debug.Log("2");
 
